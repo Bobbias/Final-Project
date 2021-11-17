5c89c7e6831956866589b9c572ef68e70afa3009
Bobbias <bobbias@gmail.com>
2021-11-17 00:58:12 -0500

    cleanup formatting, names, unused functions

--- code/final-project/src/main/java/org/comp1030/blair/Ship.java
+++ code/final-project/src/main/java/org/comp1030/blair/Ship.java
@@ -7,28 +7,35 @@ public class Ship {
     // These enumerations are public so other objects can still
     // make use of them where necessary, while keeping the declarations
     // in the class which makes the most sense.
+
+    /**
+     * 
+     */
     public enum Type {
-        CARRIER(1),
-        BATTLESHIP(2),
-        DESTROYER(4),
-        SUBMARINE(8),
-        PATROLBOAT(16);
+        CARRIER(1), BATTLESHIP(2), DESTROYER(4), SUBMARINE(8), PATROLBOAT(16);
 
         private final int value;
-        private Type(int val)
-        {
+
+        private Type(int val) {
             this.value = val;
         }
 
-        public int toValue()
-        {
+        public int toValue() {
             return this.value;
         }
 
-        public Type fromValue(int val)
-        {
-            switch (val)
-            {
+        /**
+         * fromValue maps an integer value (must be a power of 2) to a Ship.Type
+         * object.<br>
+         * Throws IllegalArgumentException if provided with an invalid value.
+         *
+         * @param val
+         *                the value to convert.
+         * @return 
+         *                one of the possible Ship.Type values.
+         */
+        public Type fromValue(int val) {
+            switch (val) {
                 case 1:
                     return CARRIER;
                 case 2:
@@ -39,47 +46,37 @@ public class Ship {
                     return SUBMARINE;
                 case 16:
                     return PATROLBOAT;
+                default:
+                    throw new IllegalArgumentException(
+                        "Argument must be a power of 2 between 1 and 16");
             }
-            throw new IllegalArgumentException("Argument must be a multiple of 2 between 1 and 16");
         }
-
-        public Type[] fromValueFlags(int flags)
-        {
-            int temp = flags;
-            int msb = 0; // r will be lg(v)
-            
-            while (temp != 0) // unroll for more speed...
-            {
-                temp >>= 1;
-                if ((temp & 1) == 1)
-                {
-                    msb++;
-                }
-            }
-            Type list[] = new Type[msb];
-            for (int i = 0; i < msb; ++i)
-            {
-                list[i] = fromValue(1 << i);
-            }
-            return list;
-        }
-    };
+    }
+    
+    /**
+     * 
+     */
     public enum Orientation {
         VERTICAL('v'),
         HORIZONTAL('h');
 
-        private final char char_value;
-        Orientation(char c)
-        {
-            this.char_value = c;
+        private final char value;
+
+        Orientation(char c) {
+            this.value = c;
         }
+        
         public char toChar() {
-            return char_value;
+            return value;
         }
-        public static Orientation fromChar(char c)
-        {
-            switch (c)
-            {
+
+        /**
+         * 
+         * @param c
+         * @return
+         */
+        public static Orientation fromChar(char c) {
+            switch (c) {
                 case 'h':
                     return HORIZONTAL;
                 case 'v':
@@ -88,16 +85,15 @@ public class Ship {
                     throw new IllegalArgumentException("Character must be 'v' or 'h'.");
             }
         }
-};
+    }
 
     // a hardcoded lookup table for the ship sizes.
-    private final int sizes[] = {5, 4, 3, 3, 2};
+    private final int[] sizes = { 5, 4, 3, 3, 2 };
     // instance variables
     private Type type;
     private int size;
-
-    private int x;
-    private int y;
+    private int shipX;
+    private int shipY;
 
     private Orientation orientation;
     
@@ -111,34 +107,41 @@ public class Ship {
         this.type = Type.CARRIER;
         this.orientation = Orientation.HORIZONTAL;
     }
+
+    /**
+     * Constructs a ship with the following parameters.
+     *
+     * @param type the type of the ship.
+     * @param x the x coordinate of the ship.
+     * @param y the y coordinate of the ship.
+     * @param direction the direction the ship is facing.
+     */
+    public Ship(Type type, int x, int y, Orientation direction) {
         this.type = type;
         // use the numeric value of the enum to look up the correct size
         this.size = sizes[type.ordinal()];
-        this.x = x;
-        this.y = y;
+        this.shipX = x;
+        this.shipY = y;
         this.orientation = direction;
     }
 
-    public int getSize()
-    {
+    public int getSize() {
         return this.size;
     }
 
-    public Type getType()
-    {
+    public Type getType() {
         return this.type;
     }
 
-    public Orientation getDirection()
-    {
+    public Orientation getDirection() {
         return this.orientation;
     }
 
     public int getX() {
-        return x;
+        return shipX;
     }
 
     public int getY() {
-        return y;
+        return shipY;
     }
 }
